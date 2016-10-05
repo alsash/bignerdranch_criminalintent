cfbadaacd0618ccf6d6a2aba4178fd05ae172d31
alsash <alsashcom@gmail.com>
2016-10-05 23:31:06 +0300

    Chapter 9

--- app/src/main/java/com/alsash/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/alsash/android/criminalintent/CrimeListFragment.java
@@ -1,6 +1,28 @@
 package com.alsash.android.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CrimeListFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.crime_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return rootView;
+    }
 }
