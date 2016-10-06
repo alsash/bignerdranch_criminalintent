5a0b2bfbde639adae961a5348c50b2f0a8c24ccf
alsash <alsashcom@gmail.com>
2016-10-06 16:11:32 +0300

    Chapter 9

--- app/src/main/java/com/alsash/android/criminalintent/ui/fragment/CrimeListFragment.java
+++ app/src/main/java/com/alsash/android/criminalintent/ui/fragment/CrimeListFragment.java
@@ -8,12 +8,22 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.alsash.android.criminalintent.R;
 
 public class CrimeListFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+        public TextView mTitleTextView;
+
+        public CrimeHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+    }
 
     @Nullable
     @Override
