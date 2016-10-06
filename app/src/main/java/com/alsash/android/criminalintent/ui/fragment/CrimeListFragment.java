2638a328426999fb68b54d743440d1dcf7680202
alsash <alsashcom@gmail.com>
2016-10-06 17:56:44 +0300

    Chapter 9

--- app/src/main/java/com/alsash/android/criminalintent/ui/fragment/CrimeListFragment.java
+++ app/src/main/java/com/alsash/android/criminalintent/ui/fragment/CrimeListFragment.java
@@ -11,22 +11,53 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.alsash.android.criminalintent.R;
+import com.alsash.android.criminalintent.data.Crime;
+import com.alsash.android.criminalintent.data.CrimeLab;
+
+import java.util.List;
 
 public class CrimeListFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
+    private CrimeAdapter mAdapter;
 
     private class CrimeHolder extends RecyclerView.ViewHolder {
-        public TextView mTitleTextView;
+        TextView mTitleTextView;
 
-        public CrimeHolder(View itemView) {
+        CrimeHolder(View itemView) {
             super(itemView);
             mTitleTextView = (TextView) itemView;
         }
     }
 
-    @Nullable
-    @Override
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+
+        private List<Crime> mCrimes;
+
+        CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new CrimeHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+            Crime crime = mCrimes.get(position);
+            holder.mTitleTextView.setText(crime.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+    }
+
+
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -35,6 +66,15 @@ public class CrimeListFragment extends Fragment {
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.crime_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        updateUi();
+
         return rootView;
+    }
+
+    private void updateUi() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mAdapter = new CrimeAdapter(crimes);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
