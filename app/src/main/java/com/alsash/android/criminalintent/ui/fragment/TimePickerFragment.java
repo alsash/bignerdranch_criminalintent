acf157df2848acd22a290c7bb6873701e6a2c62d
alsash <alsashcom@gmail.com>
2016-10-12 11:07:37 +0300

    Chapter 12. Exercise

---
+++ app/src/main/java/com/alsash/android/criminalintent/ui/fragment/TimePickerFragment.java
@@ -0,0 +1,81 @@
+package com.alsash.android.criminalintent.ui.fragment;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+
+import com.alsash.android.criminalintent.R;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class TimePickerFragment extends DialogFragment {
+
+    public static final String EXTRA_TIME = "com.alsash.android.criminalintent.date";
+
+    private static final String ARG_DATE = "date";
+
+    private DatePicker mDatePicker;
+
+    public static TimePickerFragment newInstance(Date date) {
+
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        TimePickerFragment fragment = new TimePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        View rootView = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
+        mDatePicker = (DatePicker) rootView.findViewById(R.id.dialog_date_date_picker);
+        mDatePicker.init(year, month, day, null);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(rootView)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int year = mDatePicker.getYear();
+                        int month = mDatePicker.getMonth();
+                        int day = mDatePicker.getDayOfMonth();
+                        Date date = new GregorianCalendar(year, month, day).getTime();
+                        sendResult(Activity.RESULT_OK, date);
+                    }
+                })
+                .create();
+    }
+
+    private void sendResult(int requestCode, Date date) {
+        if (getTargetFragment() == null) {
+            return;
+        }
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_TIME, date);
+        getTargetFragment().onActivityResult(getTargetRequestCode(), requestCode, intent);
+    }
+}
