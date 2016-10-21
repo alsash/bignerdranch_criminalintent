7dc0be0ac251a5a0c32316c736554b06ae1e762a
alsash <alsashcom@gmail.com>
2016-10-22 00:55:29 +0300

    Chapter 16

--- app/src/main/java/com/alsash/android/criminalintent/data/CrimeLab.java
+++ app/src/main/java/com/alsash/android/criminalintent/data/CrimeLab.java
@@ -4,12 +4,14 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 
 import com.alsash.android.criminalintent.data.database.CrimeBaseHelper;
 import com.alsash.android.criminalintent.data.database.CrimeCursorWrapper;
 import com.alsash.android.criminalintent.data.database.CrimeDbSchema.CrimeTable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -105,6 +107,15 @@ public class CrimeLab {
             return cursor.getCrime();
         } finally {
             cursor.close();
+        }
+    }
+
+    public File getPhotoFile(Crime crime) {
+        File externalFilesDir = mContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if (externalFilesDir == null) {
+            return null;
+        } else {
+            return new File(externalFilesDir, crime.getPhotoFilename());
         }
     }
 }
